c85fcd97cb3018557c79e03bca2d1e64a93bc67a
GUOED3 <EDDY.GUO@OOCL.COM>
2019-07-10 15:03:21 +0800

    commit reduce and map practice

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,7 @@
 function grouping_count(collection) {
 
   //implement here
+  return collection.reduce((key,value) => (key[value]++ || (key[value] = 1),key),{});
 }
 
 module.exports = grouping_count;
